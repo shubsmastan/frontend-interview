671136f216208d19324b4205dd2eb66748ef171b
Shoaib Mastan <114753586+shubsmastan@users.noreply.github.com>
2025-01-15 22:31:30 +0000

    Fetch data from API and display first 5 entries

--- src/Applications.tsx
+++ src/Applications.tsx
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SingleApplication from './SingleApplication';
 import { getSingleApplicationFixture } from './__fixtures__/applications.fixture';
 import styles from './Applications.module.css';
 import { Button } from './ui/Button/Button';
+import axios from 'axios';
 
 const Applications = () => {
-	const applications = getSingleApplicationFixture;
+	const [applications, setApplications] = useState([]);
+	// const applications = getSingleApplicationFixture;
+	useEffect(() => {
+		const getApplicationData = async () => {
+			try {
+				const { data } = await axios.get(
+					'http://localhost:3001/api/applications?_page=2&_limit=5'
+				);
+				setApplications(data);
+			} catch (err) {
+				console.log(
+					'There was an error fetching the application data.'
+				);
+			}
+		};
+		getApplicationData();
+	}, []);
 
 	return (
 		<div className={styles.Applications}>
-			<SingleApplication application={applications[0]} />
-			<Button
-				className={''}
-				onClick={() => {
-					// alert('hey');
+			{applications.map(app => (
+				<SingleApplication key={app.id} application={app} />
+			))}
+			<div
+				style={{
+					width: '100%',
+					display: 'flex',
+					justifyContent: 'center',
+					marginTop: '10px',
 				}}>
-				Load More
-			</Button>
+				<Button
+					className={''}
+					onClick={() => {
+						// do stuff
+					}}>
+					Load more
+				</Button>
+			</div>
 		</div>
 	);
 };

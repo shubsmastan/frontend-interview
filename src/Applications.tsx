575e3211df8f570fe4bea7c70cba199ea32dd339
Shoaib Mastan <114753586+shubsmastan@users.noreply.github.com>
2025-01-15 22:21:07 +0000

    Add button to UI

--- src/Applications.tsx
+++ src/Applications.tsx
@@ -1,16 +1,24 @@
-import React from "react";
-import SingleApplication from "./SingleApplication";
-import { getSingleApplicationFixture } from "./__fixtures__/applications.fixture";
-import styles from "./Applications.module.css";
+import React from 'react';
+import SingleApplication from './SingleApplication';
+import { getSingleApplicationFixture } from './__fixtures__/applications.fixture';
+import styles from './Applications.module.css';
+import { Button } from './ui/Button/Button';
 
 const Applications = () => {
-  const applications = getSingleApplicationFixture;
+	const applications = getSingleApplicationFixture;
 
-  return (
-    <div className={styles.Applications}>
-      <SingleApplication application={applications[0]} />
-    </div>
-  );
+	return (
+		<div className={styles.Applications}>
+			<SingleApplication application={applications[0]} />
+			<Button
+				className={''}
+				onClick={() => {
+					// alert('hey');
+				}}>
+				Load More
+			</Button>
+		</div>
+	);
 };
 
 export default Applications;
